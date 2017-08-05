b97f8d2a0e4b28927daa0e175a175a651c20fda2
Jaakko Husso <jaakko.husso@futurice.com>
2017-08-06 00:19:15 +0300

    Fix copypaste error.. 💩

--- config/prod.exs
+++ config/prod.exs
@@ -21,7 +21,7 @@ config :choicest, ChoicestWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Configure your database
-config :hello, Hello.Repo,
+config :choicest, Choicest.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
